92f27fb730971364c9cb068ab5d748e35a15047d
diegogl12 <diegogl13@hotmail.com>
2018-05-23 14:59:29 -0300

    criação de funções do teste de escopo

--- analisador_semantico_escopo.cpp
+++ analisador_semantico_escopo.cpp
@@ -33,3 +33,26 @@ void atualiza_situacao()
             get<4>(*it) = false;
     }
 }
+
+bool teste_existencia(string nome)
+{
+    for(vector< tuple<string,char,char,int,bool> > :: iterator it = analisador_escopo.begin(); it != analisador_escopo.end(); ++it)
+    {
+        if(get<0>(*it) == nome && get<4>(*it) == true)
+            return false;
+    }
+
+    return true;
+}
+
+bool teste_escopo(string nome)
+{
+    for(vector< tuple<string,char,char,int,bool> > :: iterator it = analisador_escopo.begin(); it != analisador_escopo.end(); ++it)
+    {
+        if(get<0>(*it) == nome && get<3>(*it) <= bloco_atual && get<4>(*it) == true)
+            return true;
+    }
+
+    return false;
+}
+
